fb4d0bae8ca02588f885c76d16b4b35076760429
vadim <vadim@danilekno.ru>
2023-11-17 22:58:34 +0300

    changd some stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
   def index
     if user_signed_in?
-      render json:{msg: "Основная страница"}
+      redirect_to "users/edit"
     else
       redirect_to root_path
     end
